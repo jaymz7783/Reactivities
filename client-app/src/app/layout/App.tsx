863f3356fed274143423ba111e5b3b5c398e0a45
Tony <tmitchell@greenmountaintechnology.com>
2021-03-01 11:07:57 -0600

    End section 5

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,26 +1,67 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Container } from 'semantic-ui-react'
+import { Activity } from '../models/activity'
+import NavBar from './NavBar'
+import ActivityDashboard from '../../features/activites/dashboard/ActivityDashboard'
+import { v4 as uuid } from 'uuid'
 
 function App() {
-  const [activites, setActivities] = useState([]);
+  const [activites, setActivities] = useState<Activity[]>([])
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined)
+  const [editMode, setEditMode] = useState(false)
+
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activites.find((x) => x.id === id))
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined)
+  }
+
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity()
+    setEditMode(true)
+  }
+
+  function handleFormClose() {
+    setEditMode(false)
+  }
+
+  function handleCreateOrEditActivity(activity: Activity) {
+    activity.id ? setActivities([...activites.filter((x) => x.id !== activity.id), activity]) : setActivities([...activites, { ...activity, id: uuid() }])
+    setEditMode(false)
+    setSelectedActivity(activity)
+  }
+
+  function handleDeleteActivity(id: string) {
+    setActivities([...activites.filter((x) => x.id !== id)])
+  }
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then((response) => {
-      setActivities(response.data);
-    });
-  }, []);
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then((response) => {
+      setActivities(response.data)
+    })
+  }, [])
 
   return (
-    <div>
-      <Header as='h2' icon='users' content='Reactivities' />
-      <List>
-        {activites.map((activity: any) => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
-  );
+    <>
+      <NavBar openForm={handleFormOpen} />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard
+          activities={activites}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
+    </>
+  )
 }
 
-export default App;
+export default App
